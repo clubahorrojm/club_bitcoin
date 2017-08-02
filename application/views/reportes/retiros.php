442009a68020e02fbdb8b20dfc8788334d05c6c3
Jose <solorzano202009@gmail.com>
2017-08-02 12:09:58 -0400

    Ajustes a la sección de reportes de retiros.

--- application/views/reportes/retiros.php
+++ application/views/reportes/retiros.php
@@ -62,7 +62,7 @@ redirect(base_url());
 			<div class="col-md-2">
 			    <div class="form-group">
 				<label style="font-weight:bold">&nbsp;</label><br>
-				<button type="submit" id="generar" style="font-weight: bold;font-size: 13px" class="btn btn-danger"/>
+				<button type="submit" id="generar_pdf_retiros" style="font-weight: bold;font-size: 13px" class="btn btn-danger"/>
 				&nbsp;<span class="glyphicon glyphicon-file"></span>&nbsp;Generar
 				</button>
 			    </div><!-- /.form-group -->
@@ -87,14 +87,15 @@ redirect(base_url());
 $(document).ready(function(){
       
     $('select').on({
-	change: function () {
-	    $(this).parent('div').removeClass('has-error');
-	}
+		change: function () {
+			$(this).parent('div').removeClass('has-error');
+		}
     });
+    
     $('input').on({
-	keypress: function () {
-	    $(this).parent('div').removeClass('has-error');
-	}
+		keypress: function () {
+			$(this).parent('div').removeClass('has-error');
+		}
     });
 
     $("select").select2();
@@ -102,71 +103,71 @@ $(document).ready(function(){
     $("#hasta").numeric({allow: "/"}); //Valida solo permite valores numéricos
                 
     $('#desde').datepicker({
-	format: "dd/mm/yyyy",
-	endDate: 'today',
-	minDate: "-1D",
-	maxDate: "-1D",
-	language: "es",
-	autoclose: true,
+		format: "dd/mm/yyyy",
+		endDate: 'today',
+		minDate: "-1D",
+		maxDate: "-1D",
+		language: "es",
+		autoclose: true,
     })
 		    
     $('#hasta').datepicker({
-	format: "dd/mm/yyyy",
-	endDate: 'today',
-	minDate: "-1D",
-	maxDate: "-1D",
-	language: "es",
-	autoclose: true,
+		format: "dd/mm/yyyy",
+		endDate: 'today',
+		minDate: "-1D",
+		maxDate: "-1D",
+		language: "es",
+		autoclose: true,
     })
                 
                 
 
-    $("#generar").click(function (e) {
-	e.preventDefault();  // Para evitar que se envíe por defecto
-	
-	if($('#estatus').val().trim() == '' && $('#desde').val().trim() == '' && $('#hasta').val().trim() == ''){
-	    bootbox.alert("Debe indicar algún parámetro de búsqueda", function () {
-	    }).on('hidden.bs.modal', function (event) {
-		    $('#estatus').parent('div').addClass('has-error');
-		    $('#desde').parent('div').addClass('has-error');
-		    $('#hasta').parent('div').addClass('has-error');
-	    });
-	}else if(($('#desde').val().trim() != '' && $('#hasta').val().trim() == '') || ($('#desde').val().trim() == '' && $('#hasta').val().trim() != '')){
-	    bootbox.alert("Debe completar el rango de fechas", function () {
-	    }).on('hidden.bs.modal', function (event) {
-		    $('#usuario').parent('div').addClass('has-error');
-		    $('#desde').parent('div').addClass('has-error');
-		    $('#hasta').parent('div').addClass('has-error');
-	    });
-	}else{
-	    var estatus = $('#estatus').val().trim();
-	    var desde = $('#desde').val().trim();
-	    var hasta = $('#hasta').val().trim();
-	    if (estatus == ""){
-		    estatus = "xxx";
-	    }
-	    if(desde == "" && hasta == ""){
-		    desde = "xxx";
-		    hasta = "xxx";
-	    }else{
-		    desde = desde.split("/");
-		    desde = desde[2]+"-"+desde[1]+"-"+desde[0];
-		    hasta = hasta.split("/");
-		    hasta = hasta[2]+"-"+hasta[1]+"-"+hasta[0];
-	    }
-	    // Función para validar si hay registros para la búsqueda especificada
-	    $.post('<?php echo base_url(); ?>index.php/reportes/CRepRetiros/obtenerRetiros/' + estatus + '/' + desde + '/' + hasta + '', function (response) {
-		    //~ alert(response.trim());
-		    if (response.trim() == '0') {
-				bootbox.alert("No hay registros para la consulta especificada", function () {
-				}).on('hidden.bs.modal', function (event) {    
-				});
-		    }else{
-				URL = '<?php echo base_url(); ?>index.php/reportes/CRepRetiros/pdf_retiros/' + estatus + '/' + desde + '/' + hasta + '';
-				$.fancybox.open({ padding : 0, href: URL, type: 'iframe',width: 1024, height: 860, });
-		    }
-	    });
-	} 
+    $("#generar_pdf_retiros").click(function (e) {
+		e.preventDefault();  // Para evitar que se envíe por defecto
+		
+		if($('#estatus').val().trim() == '' && $('#desde').val().trim() == '' && $('#hasta').val().trim() == ''){
+			bootbox.alert("Debe indicar algún parámetro de búsqueda", function () {
+			}).on('hidden.bs.modal', function (event) {
+				$('#estatus').parent('div').addClass('has-error');
+				$('#desde').parent('div').addClass('has-error');
+				$('#hasta').parent('div').addClass('has-error');
+			});
+		}else if(($('#desde').val().trim() != '' && $('#hasta').val().trim() == '') || ($('#desde').val().trim() == '' && $('#hasta').val().trim() != '')){
+			bootbox.alert("Debe completar el rango de fechas", function () {
+			}).on('hidden.bs.modal', function (event) {
+				$('#usuario').parent('div').addClass('has-error');
+				$('#desde').parent('div').addClass('has-error');
+				$('#hasta').parent('div').addClass('has-error');
+			});
+		}else{
+			var estatus = $('#estatus').val().trim();
+			var desde = $('#desde').val().trim();
+			var hasta = $('#hasta').val().trim();
+			if (estatus == ""){
+				estatus = "xxx";
+			}
+			if(desde == "" && hasta == ""){
+				desde = "xxx";
+				hasta = "xxx";
+			}else{
+				desde = desde.split("/");
+				desde = desde[2]+"-"+desde[1]+"-"+desde[0];
+				hasta = hasta.split("/");
+				hasta = hasta[2]+"-"+hasta[1]+"-"+hasta[0];
+			}
+			// Función para validar si hay registros para la búsqueda especificada
+			$.post('<?php echo base_url(); ?>index.php/reportes/CRepRetiros/obtenerRetiros/' + estatus + '/' + desde + '/' + hasta + '', function (response) {
+				//~ alert(response.trim());
+				if (response.trim() == '0') {
+					bootbox.alert("No hay registros para la consulta especificada", function () {
+					}).on('hidden.bs.modal', function (event) {    
+					});
+				}else{
+					URL = '<?php echo base_url(); ?>index.php/reportes/CRepRetiros/pdf_retiros/' + estatus + '/' + desde + '/' + hasta + '';
+					$.fancybox.open({ padding : 0, href: URL, type: 'iframe',width: 1024, height: 860, });
+				}
+			});
+		}
     });
 });
 </script>
